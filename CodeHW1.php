00c2eda411aefbdef25b496e7f904c8761f6ca9b
Zane Castillo <58093234+considine157@users.noreply.github.com>
2020-02-11 12:10:31 -0500

    Create CodeHW1.php

---
+++ CodeHW1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<title>Week 2</title>
+<style>
+	body{
+		background-color: green;
+		font-family: Helvetica;
+		text-align: center;
+		text-color: blue;
+}
+
+	
+</style>
+
+<h1>Week 3 examples</h1>
+<p>This is my work.</p>
+
+<p>
+<?php
+#
+?>
+
+</p>
+
+<h2>Fun Class</h2>
+<p>So much to learn.</p>
+</body>
+</html>
