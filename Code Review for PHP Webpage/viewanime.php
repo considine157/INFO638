421a04424dc88d0eb1cac0c443abe514bfd4ef26
Zane Castillo <zanecastillo3@gmail.com>
2020-05-01 19:25:58 -0400

    Update viewanime.php
    fixed issues

--- Code Review for PHP Webpage/viewanime.php
+++ Code Review for PHP Webpage/viewanime.php
@@ -10,9 +10,9 @@ if ($connection->connect_error) die($connection->connect_error);
 
 #Search the anime table for anime titles and also using '%' to find letters/characters similar to what is entered.
 if (isset($_GET['anime_title'])) {
-	$anime_title = sanitizeMySQL($conn, $_GET['anime_title']);
+	$anime_title = sanitizeMySQL($connection, $_GET['anime_title']);
 	$search = "SELECT * FROM anime WHERE anime_title LIKE  '%$anime_title%'";
-	$result = $conn->query($search);
+	$result = $connection->query($search);
 	if (!$result) die ("Invalid anime title.");
 	$rows = $result->num_rows;
 	if ($rows == 0) {
